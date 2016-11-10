995f6e932ace508f604cdc43789a7b77d2528511
Andrey Panchenko <asfdfdfd@gmail.com>
2016-11-10 15:31:42 +0300

    Carthage support.

--- BGTableViewRowActionWithImage.h
+++ BGTableViewRowActionWithImage.h
@@ -8,6 +8,12 @@
 
 #import <UIKit/UIKit.h>
 
+//! Project version number for BGTableViewRowActionWithImage.
+FOUNDATION_EXPORT double BGTableViewRowActionWithImageVersionNumber;
+
+//! Project version string for BGTableViewRowActionWithImage.
+FOUNDATION_EXPORT const unsigned char BGTableViewRowActionWithImageVersionString[];
+
 @interface BGTableViewRowActionWithImage : UITableViewRowAction
 
 + (instancetype)rowActionWithStyle:(UITableViewRowActionStyle)style title:(NSString *)title backgroundColor:(UIColor *)backgroundColor image:(UIImage *)image forCellHeight:(NSUInteger)cellHeight handler:(void (^)(UITableViewRowAction *, NSIndexPath *))handler;
